90bac4cb25e149bd978994917665b7bbcdfe24bf
MC.Vishnya <MC.Vishnya>
2025-01-22 23:37:04 +0500

    Added another test

---
+++ lecture3.py
@@ -0,0 +1,24 @@
+'''
+    Problem:
+    Find sum of the first N elements.
+
+    Objective function:
+    f(i) is the sum of the first i elements.
+
+    Recurrence relation:
+    f(n) = f(n - 1) + n
+
+'''
+
+# n = 0 
+def nSum(n: int) -> int:
+    dp: list = [0] * (n + 1)
+    dp[0] = 0
+    for i in range(1, n + 1):
+        dp[i] = dp[i - 1] + i
+    return dp[n]
+
+print(nSum(0))
+print(nSum(1))
+print(nSum(5))
+print(nSum(15))
